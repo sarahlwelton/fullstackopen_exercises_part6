4b75824e95a61783c93af9487ad52a7727353de2
Sarah Emmett <sarah.welton@couchbase.com>
2025-06-20 13:51:12 -0400

    Exercise 6.10 anecdotes Step 8: Use Redux Toolkit to create the store and use createSlice for reducers and action creators

--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,17 +21,20 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch(action.type) {
-
-    case 'NEW_ANECDOTE': 
-      return [...state, action.payload]
-    
-    case 'VOTE': {
-      const id = action.payload.id
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        id: getId(),
+        votes: 0
+      })
+    },
+    vote(state, action) {
+      const id = action.payload
       
       const anecdoteToChange = state.find(a => a.id === id)
 
@@ -42,29 +47,8 @@ const anecdoteReducer = (state = initialState, action) => {
         .map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
         .sort((a, b) => b.votes - a.votes)
     }
-    
-    default:
-      return state
   }
-}
+})
 
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: getId(),
-      votes: 0
-    }
-  }
-}
-
-export const vote = (id) => {
-    console.log('vote', id)
-    return {
-      type: 'VOTE',
-      payload: { id }
-    }
-  }
-
-export default anecdoteReducer;
+export const { createAnecdote, vote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer;
--- anecdotes/src/reducers/filterReducer.js
+++ anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,18 @@
-const filterReducer = (state = '', action) => {
-  console.log(state)
+import { createSlice } from '@reduxjs/toolkit'
 
-  switch(action.type) {
-    case 'SET_FILTER':
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
       return action.payload
-    default:
-      return state
+    },
+    filterChange(state, action) {
+      return action.payload
+    }
   }
-}
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
 
-export default filterReducer;
+export const { setFilter, filterChange } = filterSlice.actions
+export default filterSlice.reducer;
