b3cd816bdf0f01cd1551332f0de58bee9a77f256
Sarah Emmett <sarah.welton@couchbase.com>
2025-07-25 13:21:32 -0400

    Exercise 6.22 react query anecdotes Step 3: Implement voting for anecdotes with React Query

--- anecdotes-react-query/src/App.jsx
+++ anecdotes-react-query/src/App.jsx
@@ -1,13 +1,27 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateVotes } from './requests'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+  const updateVotesMutation = useMutation({
+    mutationFn: updateVotes,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      const updatedAnecdotes = anecdotes.map(anecdote =>
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      )
+
+      queryClient.setQueryData(['anecdotes'], updatedAnecdotes)
+    }
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateVotesMutation.mutate({...anecdote, votes: anecdote.votes + 1 })
   }
 
   const result = useQuery(
